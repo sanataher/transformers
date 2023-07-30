8761ecb859529a85e3cf1f57a9b1b5c608dca7b3
sanataher <negin_taherpoor@yahoo.com>
2023-07-30 20:42:13 +0330

    Update modeling_roberta.py

--- src/transformers/models/roberta/modeling_roberta.py
+++ src/transformers/models/roberta/modeling_roberta.py
@@ -1437,7 +1437,7 @@ class RobertaClassificationHead(nn.Module):
         x = features[:, 0, :]  # take <s> token (equiv. to [CLS])
         x = self.dropout(x)
         x = self.dense(x)
-        relu = nn.ReLU()#sana
+        relu = nn.ReLU(True)#sana
         x = relu(x) #sana
         x = self.dropout(x)
         x = self.out_proj(x)
